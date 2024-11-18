36f527e930ab876202ed637f46074c799db0126a
SaskiaTe <saskia.tellenbach16@gmail.ch>
2024-11-18 09:27:44 +0100

    Übung 3 gelöst Ende Lektion 1

--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
 func main() {
-	var eyes = rand.Intn(5) + 1
+	var eyes = rand.Intn(5) + 1 
 	var when = time.Now()
 
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice shows", eyes, "eyes")
+	eyesFile, _ := os.Create("eyes.txt")    
+	defer eyesFile.Close()
+	diceFile, _ := os.Create("eyes.txt")    
+	defer diceFile.Close()    
 
-	// TODO: use fmt.Fprintln instead!
+	fmt.Fprintln(eyesFile, "The dice shows", eyes, "eyes")    
+	fmt.Fprintln(diceFile, "The dice was rolled at", when)
+
+    fmt.Println("the dice shows", eyes, "eyes") 
 	fmt.Println("the dice was rolled at", when)
+} 
 
-	// TODO: how to write the output into eyes.txt and dice.log?
-	// go run ex3/main.go TODO
-}
