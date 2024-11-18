883be6ed802b96ab4e99cf112a0a769adba9b312
SaskiaTe <saskia.tellenbach16@gmail.ch>
2024-11-18 09:02:00 +0100

    Übung 1.1 bearbeitet und go richtig installiert

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,14 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName string = "Saskia"
+	var lastName string = "Tellenbach"
+	var dayOfBirth int = 16
+	var monthOfBirth int = 2
+	var yearOfBirth int = 2007
+	var numberOfSiblings int = 1
+	var heightInMeters float32 = 1.73
+	var zodiacSign rune = '\u2652'
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
