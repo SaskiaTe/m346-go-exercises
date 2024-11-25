53852a64d818d2d31c3c8de28bbd2a5e6377e249
SaskiaTe <saskia.tellenbach16@gmail.ch>
2024-11-25 08:35:22 +0100

    Übung 2.1 gelöst

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -3,26 +3,32 @@ package main
 import "fmt"
 
 type FullName struct {
-	// TODO: add fields
+	Firstname string
+	LastName  string
 }
 
-// TODO: declare a structure for birth date
+type BirthDate struct {
+	DateOfBirth string
+}
 
 type Profile struct {
-	// TODO: embed full name and birth date information
+	FullName
+	BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
 
 func main() {
 	var me = Profile{
-		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		NumberOfSiblings: 1,
+		ZodiacSign:       '\u2652',
+		BirthDate:        BirthDate{DateOfBirth: "16.02.2007"},
+		FullName: FullName{Firstname: "Saskia",
+			LastName: "Tellenbach"},
 	}
+
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
-	// TODO: imagine, you get a little brother or sister
-	fmt.Println("Siblings After:", me.NumberOfSiblings)
+	fmt.Println("Siblings After:", me.NumberOfSiblings+1)
 }
