31a16e79389606b8d453be34cc97143684f06d36
SaskiaTe <saskia.tellenbach16@gmail.ch>
2024-11-25 09:22:04 +0100

    Übung 2.4 gelöst

--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,37 @@
 package main
 
+import "fmt"
+
 func main() {
-	// TODO: declare a type for Student (with first and last name)
-	// TODO: declare a type for Class (consisting of multiple students)
-	// TODO: declare a map of modules being attended by multiple classes
-	// TODO: output everything using fmt.Println()
+	type student struct{
+		Firstname string
+		LastName string
+	}
+
+	s1 := student{Firstname: "Saskia", LastName: "Tellenbach"}
+	s2 := student{Firstname: "Lea", LastName: "Schürch"}
+	s3 := student{Firstname: "Tobias", LastName: "Clausen"}
+	s4 := student{Firstname: "Lenny", LastName: "Amstutz"}
+	s5 := student{Firstname: "Nicola", LastName: "Ming"}
+	s6 := student{Firstname: "Colin", LastName: "Wütschert"}
+	s7 := student{Firstname: "Lia", LastName: "Ditli"}
+
+	type class []student
+	
+	modules := map[string][]class{
+		"M320": {
+			{s1, s2, s3, s4},
+			{s5, s6, s7},
+		},
+		"M346": {
+			{s1, s2, s3, s4},
+			{s5, s6, s7},
+		},
+		"M254": {
+			{s5, s6, s7},
+		},
+	}
+
+	fmt.Println(modules)
+
 }
