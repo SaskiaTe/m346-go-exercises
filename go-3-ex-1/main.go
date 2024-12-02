ec3c1ab7635b83a1ba370040451d24057875c8a6
SaskiaTe <saskia.tellenbach16@gmail.ch>
2024-12-02 08:15:30 +0100

    Übung 3.1 gelöst

--- go-3-ex-1/main.go
+++ go-3-ex-1/main.go
@@ -19,9 +19,31 @@ const (
 
 func outputWithZodiacSign(p Person) {
 	var zodiacSign rune = '?'
-
-	// TODO: Assign proper value to zodiacSign using if/else branching.
-	// NOTE: The runes are defined above as constants.
+	if (p.BirthDate.Month == 3 && p.BirthDate.Day >= 21) || (p.BirthDate.Month == 4 && p.BirthDate.Day <= 19) {
+		zodiacSign = '\u2648'
+	} else if (p.BirthDate.Month == 4 && p.BirthDate.Day >= 20) || (p.BirthDate.Month == 5 && p.BirthDate.Day <= 20) {
+		zodiacSign = '\u2649'
+	} else if (p.BirthDate.Month == 5 && p.BirthDate.Day >= 21) || (p.BirthDate.Month == 6 && p.BirthDate.Day <= 20) {
+		zodiacSign = '\u264a'
+	} else if (p.BirthDate.Month == 6 && p.BirthDate.Day >= 21) || (p.BirthDate.Month == 7 && p.BirthDate.Day <= 22) {
+		zodiacSign = '\u264b' 
+	} else if (p.BirthDate.Month == 7 && p.BirthDate.Day >= 23) || (p.BirthDate.Month == 8 && p.BirthDate.Day <= 22) {
+		zodiacSign = '\u264c'
+	} else if (p.BirthDate.Month == 8 && p.BirthDate.Day >= 23) || (p.BirthDate.Month == 9 && p.BirthDate.Day <= 22) {
+		zodiacSign = '\u264d'
+	} else if (p.BirthDate.Month == 9 && p.BirthDate.Day >= 23) || (p.BirthDate.Month == 10 && p.BirthDate.Day <= 22) {
+		zodiacSign = '\u264e'
+	} else if (p.BirthDate.Month == 10 && p.BirthDate.Day >= 23) || (p.BirthDate.Month == 11 && p.BirthDate.Day <= 21) {
+		zodiacSign = '\u264f'
+	} else if (p.BirthDate.Month == 11 && p.BirthDate.Day >= 22) || (p.BirthDate.Month == 12 && p.BirthDate.Day <= 21) {
+		zodiacSign = '\u2650'
+	} else if (p.BirthDate.Month == 12 && p.BirthDate.Day >= 22) || (p.BirthDate.Month == 1 && p.BirthDate.Day <= 19) {
+		zodiacSign = '\u2651'
+	} else if (p.BirthDate.Month == 1 && p.BirthDate.Day >= 20) || (p.BirthDate.Month == 2 && p.BirthDate.Day <= 18) {
+		zodiacSign = '\u2652'
+	} else if (p.BirthDate.Month == 2 && p.BirthDate.Day >= 19) || (p.BirthDate.Month == 3 && p.BirthDate.Day <= 20) {
+		zodiacSign = '\u2653'
+	}
 
 	fmt.Printf("%s %s, born on %02d.%02d.%04d, has the zodiac sign %c.\n",
 		p.FirstName, p.LastName, p.Day, p.Month, p.Year, zodiacSign)
