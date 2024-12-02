aa10b721b7bbd1de5f4f250cb17a3358bb443f54
SaskiaTe <saskia.tellenbach16@gmail.ch>
2024-12-02 08:45:44 +0100

    Übung 4.1 gelöst

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,13 @@
 package main
+import "fmt"
 
-// TODO: implement the function computeGrade
+func computeGrade(gotPoints int, maxPoints int) float64 {
+	grade := float64(gotPoints) / float64(maxPoints) * 5 + 1
+	return grade;
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	fmt.Println(computeGrade(45, 100)) 
+	fmt.Println(computeGrade(80, 100)) 
+	fmt.Println(computeGrade(57, 60)) 
 }
