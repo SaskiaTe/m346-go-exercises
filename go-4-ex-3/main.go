2ea548e770f8f472ae56333a96f1b108dc8a94f3
SaskiaTe <saskia.tellenbach16@gmail.ch>
2024-12-09 08:40:19 +0100

    Übung 4.3 verbessert

--- go-4-ex-3/main.go
+++ go-4-ex-3/main.go
@@ -6,33 +6,22 @@ import(
 )
 
 func computeQuadraticFormula(a, b, c float64) []float64 {
-	
 	D := (b*b -4*a*c)
 
 	if D > 0 {
 		root1 := (-b + math.Sqrt(D)) / (2 * a)
 		root2 := (-b - math.Sqrt(D)) / (2 * a)
 		return []float64{root1, root2};
-
 	}else if D == 0{
 		root1 := (-b + math.Sqrt(D)) / (2 * a)
 		return []float64{root1};
 	}else {
 		return []float64{};
 	}
-	
 }
 
 func main() {
-	a1, b1, c1 := 3.0, 4.0, 1.0
-	solutions1 := computeQuadraticFormula(a1, b1, c1)
-	fmt.Printf("Lösungen für a=%.1f, b=%.1f, c=%.1f: %v\n", a1, b1, c1, solutions1)
-
-	a2, b2, c2 := 2.0, 4.0, 2.0
-	solutions2 := computeQuadraticFormula(a2, b2, c2)
-	fmt.Printf("Lösungen für a=%.1f, b=%.1f, c=%.1f: %v\n", a2, b2, c2, solutions2)
-
-	a3, b3, c3 := 3.0, 4.0, 2.0
-	solutions3 := computeQuadraticFormula(a3, b3, c3)
-	fmt.Printf("Lösungen für a=%.1f, b=%.1f, c=%.1f: %v\n", a3, b3, c3, solutions3)
+	fmt.Println(computeQuadraticFormula(3, 4, 1))
+	fmt.Println(computeQuadraticFormula(2, 4, 2))
+	fmt.Println(computeQuadraticFormula(3, 4, 2))
 }
